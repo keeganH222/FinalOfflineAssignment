b07ebdc6c80d86b5f9cfc97f35d7a9b65d0abcd8
Keegan Haasbeek <keeganhaasbeek@outlook.com>
2019-05-26 21:23:25 -0500

    changing cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'resturantCacheV10';
+const staticCacheName = 'resturantCacheV11';
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(staticCacheName).then((cache) => {
